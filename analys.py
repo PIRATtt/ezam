cca8a6c6ef78d85771f97358f7b7b48d182c0a88
PIRATtt <77304210+PIRATtt@users.noreply.github.com>
2023-04-10 19:08:20 +0300

    chance analys

--- analys.py
+++ analys.py
@@ -6,7 +6,7 @@ res_code = float(pylint.split('\n')[-3].split()[6].split('/')[0])
 if res_code < 8:
     print('Error code')
 print('\n\nNew commit grace to GitFlow!!!\n\n')
-os.system('bandit -iii -lll -q -r ./skachki/ -o bandit.json -f json')
+os.system('bandit -iii -lll -q -r ./ezam/ -o bandit.json -f json')
 with open('bandit.json', 'r', encoding='utf-8') as f:
     res = json.load(f)
 if res['results']:
